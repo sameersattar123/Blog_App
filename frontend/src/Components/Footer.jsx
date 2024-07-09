8f721c20adfb32aea09265ccb71c390e9ec48deb
Sameer <sameersattar1111@gmail.com>
2024-07-10 02:45:48 +0500

    footer start

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <ul className="footer_category">
+        <li>
+          <Link to={"/posts/categories/Agriculture"}>Agriculture</Link>
+        </li>
+        <li>
+          <Link to={"/posts/categories/Bussines"}>Bussines</Link>
+        </li>
+        <li>
+          <Link to={"/posts/categories/Education"}>Education</Link>
+        </li>
+        <li>
+          <Link to={"/posts/categories/Entertainment"}>Entertainment</Link>
+        </li>
+        <li>
+          <Link to={"/posts/categories/Weather"}>Weather</Link>
+        </li>
+        <li>
+          <Link to={"/posts/categories/Art"}>Art</Link>
+        </li>
+        <li>
+          <Link to={"/posts/categories/Uncategorized"}>Uncategorized</Link>
+        </li>
+        <li>
+          <Link to={"/posts/categories/Investment"}>Investment</Link>
+        </li>
+      </ul>
+      <div className="footer_copyright">
+        <small>All Rights Reserved &copy; Copyright, Sameer Sattar</small>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+// import logo from "../assets/logo.png"
+// import { FaBars } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <nav>
+      <div className="container nav_container">
+        <Link to={"/"} className="nav_logo">
+          <img src='https://yt3.googleusercontent.com/nwyG0ql8sxARogaFb3sgpjoNd86v7hL9W2eeEysP2LryigBtYtX-J2Mw9H1lpcB9D4sw0aCx9w=s900-c-k-c0x00ffffff-no-rj' alt="" />
+        </Link>
+        <ul className="nav_menu">
+          <li>
+            <Link to={"/profile"} className="nav_link">
+              Ernest Achiever
+            </Link>
+          </li>
+          <li>
+            <Link to={"/create"} className="nav_link">
+              Create Post
+            </Link>
+          </li>
+          <li>
+            <Link to={"/authors"} className="nav_link">
+              Authors
+            </Link>
+          </li>
+          <li>
+            <Link to={"/logout"} className="nav_link">
+              Logout
+            </Link>
+          </li>
+        </ul>
+        <button className="nav_toggle_btn">
+            <AiOutlineClose/>
+        </button>
+      </div>
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
