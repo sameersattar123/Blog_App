a9a1065f52127354383a4b00936d1ac4eb38bed2
Sameer <sameersattar1111@gmail.com>
2024-07-10 06:35:29 +0500

    complete

--- frontend/src/Pages/AuthorPosts.jsx
+++ frontend/src/Pages/AuthorPosts.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PostItem from '../Components/PostItem';
+import { DUMMY_DATA } from '../data';
 
 const AuthorPosts = () => {
+  const [posts, setPosts] = useState(DUMMY_DATA);
   return (
-    <div>AuthorPosts</div>
+    <section className="posts">
+      <div className="continer author-posts-container">
+        { posts.length > 0 ? posts.map((post, i) => {
+          return (
+            <PostItem
+              key={i}
+              postID={post.id}
+              title={post.title}
+              category={post.category}
+              description={post.desc}
+              authorID={post.authorID}
+              thumbnail={post.thumbnail}
+            />
+          );
+        }) : <h2 className="center">No Posts Found</h2>
+      } 
+      </div>
+    </section>
   )
 }
 
