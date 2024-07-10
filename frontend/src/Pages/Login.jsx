34f9db23f4d216b8cc0abff5a1d13cecd7fe028e
Sameer <sameersattar1111@gmail.com>
2024-07-10 10:47:56 +0500

    complete login register page

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+  
+  const changeInputHandle = (e) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+  };
+  console.log(userData)
   return (
-    <div>Login</div>
-  )
-}
+    <section className="login">
+      <div className="container">
+        <h2>Sign In</h2>
+        <form action="" className="login_form form">
+          <p className="form_error_message">This is an error message</p>
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={userData.email}
+            onChange={changeInputHandle}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={userData.password}
+            onChange={changeInputHandle}
+            required
+          />
+          <button className="btn primary" type="submit">Register</button>
+        </form>
+        <small>Don't Have an Account? <Link to={'/register'}>Sign Up</Link></small>
+      </div>
+    </section>
+  );
+};
 
-export default Login
+export default Login;
--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    comfirmPassword : ''
+  });
+  
+  const changeInputHandle = (e) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+  };
+  console.log(userData)
   return (
-    <div>Register</div>
-  )
-}
+    <section className="register">
+      <div className="container">
+        <h2>Sign Up</h2>
+        <form action="" className="register_form form">
+          <p className="form_error_message">This is an error message</p>
+          <input
+            type="text"
+            placeholder="Full Name"
+            name="name"
+            value={userData.name}
+            onChange={changeInputHandle}
+            required
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={userData.email}
+            onChange={changeInputHandle}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={userData.password}
+            onChange={changeInputHandle}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Comfirm Passwrod"
+            name="comfirmPassword"
+            value={userData.comfirmPassword}
+            onChange={changeInputHandle}
+            required
+          />
+          <button className="btn primary" type="submit">Register</button>
+        </form>
+        <small>Already Have an Account? <Link to={'/login'}>Sign In</Link></small>
+      </div>
+    </section>
+  );
+};
 
-export default Register
+export default Register;
