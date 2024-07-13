c9040ce2ddcc5b204260c087415a8253aa82c435
Sameer <sameersattar1111@gmail.com>
2024-07-13 06:30:09 +0500

    complete frontend

--- frontend/src/Pages/EditPost.jsx
+++ frontend/src/Pages/EditPost.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from "react";
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
 
 const EditPost = () => {
-  return (
-    <div>EditPost</div>
-  )
-}
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("Uncategorized");
+  const [thumbnail, setThumbnail] = useState("");
+  const [description, setDescription] = useState("");
 
-export default EditPost
+  const POST_CATEGORIES = [
+    "Agriculture",
+    "Bussiness",
+    "Education",
+    "Entertainment",
+    "Art",
+    "Investment",
+    "Uncategorized",
+    "Weather",
+  ];
+  return (
+    <section className="create_post">
+      <div className="container">
+        <h2>Edit Post</h2>
+        <p className="form_error_message">This is an error message</p>
+        <form className="form create_post_form" action="">
+          <input
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            placeholder="Title"
+            autoFocus
+          />
+          <select
+            name="category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            {POST_CATEGORIES.map((cat) => (
+              <option key={cat}>{cat}</option>
+            ))}
+          </select>
+          <ReactQuill
+            value={description}
+            onChange={(value) => setDescription(value)}
+            className="ql_react"
+            />
+            <input
+              type="file"
+              onChange={(e) => setThumbnail(e.target.files[0])}
+              accept="jpg , png , jpeg"
+            />
+            <button className="btn primary" type="submit">
+              Update
+            </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default EditPost;
